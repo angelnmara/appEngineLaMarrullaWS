130bf222937372221120214600be5caabef6f867
jose david rincon angeles <angelnmara@hotmail.com>
2020-02-27 18:34:43 -0600

    se agrega filter sin funcionalidad

--- src/main/java/com/lamarrulla/implement/AuthenticationFilter.java
+++ src/main/java/com/lamarrulla/implement/AuthenticationFilter.java
@@ -3,6 +3,12 @@ package com.lamarrulla.implement;
 import java.io.IOException;
 
 import javax.annotation.Priority;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -16,7 +22,8 @@ import com.lamarrulla.security.Token;
 @Secured
 @Provider
 @Priority(Priorities.AUTHENTICATION)
-public class AuthenticationFilter implements ContainerRequestFilter {
+public class AuthenticationFilter implements Filter {
+	
 	private static final String REALM = "example";
     private static final String AUTHENTICATION_SCHEME = "Bearer";
     private static final String Usuario = "username";
@@ -26,9 +33,10 @@ public class AuthenticationFilter implements ContainerRequestFilter {
     String tok;
     String user;
     String secret;
-    ContainerRequestContext requestCont;
+//    ContainerRequestContext requestCont;
+    ServletRequest requestCont;
 
-	@Override
+/*	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		// Get the Authorization header from the request
 		requestCont = requestContext;
@@ -56,16 +64,16 @@ public class AuthenticationFilter implements ContainerRequestFilter {
         } catch (Exception e) {
             abortWithUnauthorized(requestContext);
         }	
-	}
+	}*/
 	
-	private boolean isTokenBasedAuthentication(String authorizationHeader) {
-
-        // Check if the Authorization header is valid
-        // It must not be null and must be prefixed with "Bearer" plus a whitespace
-        // The authentication scheme comparison must be case-insensitive
-        return authorizationHeader != null && authorizationHeader.toLowerCase()
-                    .startsWith(AUTHENTICATION_SCHEME.toLowerCase() + " ");
-    }
+//	private boolean isTokenBasedAuthentication(String authorizationHeader) {
+//
+//        // Check if the Authorization header is valid
+//        // It must not be null and must be prefixed with "Bearer" plus a whitespace
+//        // The authentication scheme comparison must be case-insensitive
+//        return authorizationHeader != null && authorizationHeader.toLowerCase()
+//                    .startsWith(AUTHENTICATION_SCHEME.toLowerCase() + " ");
+//    }
 	
 	private void abortWithUnauthorized(ContainerRequestContext requestContext) {
 
@@ -87,11 +95,56 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 			token.setUser(user);
 			token.VerifyToken();
 			if(!token.isAutenticado()) {
-				abortWithUnauthorized(requestCont);
+//				abortWithUnauthorized(requestCont);
 				return;
 			}
 		}catch(Exception ex) {
 			System.out.println(ex.getMessage());
 		}		
     }
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("prueba init filter");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// Get the Authorization header from the request
+		requestCont = request;
+		System.out.println("prueba dofilter filter");
+//        String authorizationHeader =
+//                request.getHeaderString(HttpHeaders.AUTHORIZATION);
+//
+//        // Validate the Authorization header
+//        if (!isTokenBasedAuthentication(authorizationHeader)) {
+//            abortWithUnauthorized(requestContext);
+//            return;
+//        }
+//
+//        // Extract the token from the Authorization header
+//        tok = authorizationHeader.substring(AUTHENTICATION_SCHEME.length()).trim();
+//        
+//        user = requestContext.getHeaderString(Usuario);
+//        
+//        secret = requestContext.getHeaderString(Secret);
+//
+//        try {
+//
+//            // Validate the token
+//            validateToken();
+//
+//        } catch (Exception e) {
+//            abortWithUnauthorized(requestContext);
+//        }
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
 }
